1d4fda6a51a441ddc68123128707a400b335c4da
Lucas Ferrari <lucas.n.ferrari@outlook.com>
2024-10-15 17:52:01 -0300

    chore: fake auth

--- app/entrypoints/api/entrypoints.py
+++ app/entrypoints/api/entrypoints.py
@@ -1,10 +1,15 @@
 from flask import Blueprint
 from app.domain.commands.handlers import handler_get_popular_movies
+from app.mock_auth import check_token
 
 movies_bp = Blueprint("movies", __name__, url_prefix="/movies")
 
 
 @movies_bp.route("/popular", methods=["GET"])
 def get_popular_movies():
+    user = check_token()
+    if isinstance(user, tuple):
+        return user
+
     res_movies = handler_get_popular_movies()
     return res_movies
