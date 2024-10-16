f519de7f163c8fe13d96ccb859d01fdb8901c702
Lucas Ferrari <lucas.n.ferrari@outlook.com>
2024-10-16 13:42:52 -0300

    get tmdb apikey from env

--- app/config.py
+++ app/config.py
@@ -1,10 +1,15 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Config:
-    DEBUG = True
-    TMDB_API_KEY = ""
+    DEBUG = False
+    TMDB_API_KEY = os.getenv("TMDB_API_KEY", "")
     TMDB_BASE_URL = "https://api.themoviedb.org/3/"
     REDIS_URL = "redis://127.0.0.1:6379/0"
     CACHE_TYPE = "redis"
     CACHE_DEFAULT_TIMEOUT = 30
+    SQLALCHEMY_DATABASE_URI = "sqlite:///movies.db"
