cb7f32b181908e3c391ba2acae9ac1aea13f6690
Lucas Ferrari <lucas.n.ferrari@outlook.com>
2024-10-16 01:18:48 -0300

    tweak users data structure to cleaner mock_auth

--- app/mock_auth.py
+++ app/mock_auth.py
@@ -1,9 +1,9 @@
 from flask import jsonify, request
 
-USERS = {
-    "1": {"token": "abcdef1234567890", "role": "ADMIN"},
-    "2": {"token": "1234567890", "role": "USER"},
-}
+USERS = [
+    {"id": 1, "token": "abcdef1234567890", "role": "ADMIN"},
+    {"id": 2, "token": "1234567890", "role": "USER"},
+]
 
 
 def check_token():
@@ -12,7 +12,7 @@ def check_token():
         return jsonify({"error": "Missing token"}), 401
 
     token = auth_header.split()[1]
-    user = next((u for u in USERS.values() if u["token"] == token), None)
+    user = next((u for u in USERS if u["token"] == token), None)
     if not user:
         return jsonify({"error": "Invalid token"}), 401
 
